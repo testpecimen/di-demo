23fc314aadd2cead24ca0608fc0bcd65392423d5
Diego Junco <testpecimen@gmail.com>
2018-03-14 16:15:08 -0500

    Hello method

--- src/main/java/diegojunco/springframework/didemo/controllers/MyController.java
+++ src/main/java/diegojunco/springframework/didemo/controllers/MyController.java
@@ -1,7 +1,15 @@
 package diegojunco.springframework.didemo.controllers;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by Diego Junco on 14/03/2018
  **/
+@Controller
 public class MyController {
+
+    public String hello(){
+        System.out.println("Hello!!! ");
+        return "foo";
+    }
 }
