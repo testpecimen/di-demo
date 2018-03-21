2b7d15dc69422464696dbc76abb4e7c757cfafc1
Diego Junco <testpecimen@gmail.com>
2018-03-21 11:22:58 -0500

    Dependency injection. German language @Profile annotation

--- src/main/java/diegojunco/springframework/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/diegojunco/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
- * Created by Diego Junco on 15/03/2018
+ * Created by Diego Junco on 20/03/2018
  **/
 @Service
 @Primary
-@Profile({"en","default"})
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting service";
+        return "Hallo - primarer BengruBungsdienst";
     }
 }
